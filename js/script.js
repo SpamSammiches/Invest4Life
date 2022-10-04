598ef796ec360fffe36ad10256c2ec2ccd03fb74
vanhauml <van_ha@student.uml.edu>
2022-10-04 15:34:32 -0400

    add navbar

--- js/script.js
+++ js/script.js
@@ -1,39 +1,59 @@
+// Calculates ending balance
 function calcFV() {
-    if (!validateForm()) {
-        alert("All fields must be filled in.")
+    if (!validateForm("fv-fv")) {
+        alert("Fill in all required fields.")
         return false;
-    }
-
-    let rate = document.getElementById("rate").value;
+    } 
+ 
+    let rate = document.getElementById("fv-rate").value;
     rate /= 100;
-    let years = document.getElementById('years').value;
-    let contribution = document.getElementById("contribution").value;
-    let pv = document.getElementById('pv').value;
-    var fv = contribution * (Math.pow((1+rate), years) - 1)/rate + pv * Math.pow((1+rate), years);
-
-    document.getElementById("form-result").textContent = "";
-    var newNode = document.createElement("h1");
-    newNode.textContent = "Balance At Retirement";
-    document.getElementById('form-result').append(newNode);
-    newNode = document.createElement("h1")
-    var num = "$" + Number(fv.toFixed(2)).toLocaleString();
-    newNode.textContent = num;
-    document.getElementById("form-result").append(newNode);
+    let years = document.getElementById('fv-years').value;
+    let contribution = document.getElementById("fv-contribution").value;
+    let pv = document.getElementById('fv-pv').value;
+    let fv = contribution * (Math.pow((1+rate), years) - 1)/rate + pv * Math.pow((1+rate), years);
+    let num = Number(fv.toFixed(2)).toLocaleString();
+    document.getElementById("fv-fv").placeholder = num;
     return false;
 }
 
-function validateForm() {
-    let rate = document.getElementById("rate").value;
-    let years = document.getElementById('years').value;
-    let contribution = document.getElementById("contribution").value;
-    let pv = document.getElementById('pv').value;
-
-    if (rate === null || rate === "" 
-        || years === null || years ===""
-        || contribution === null || contribution === ""
-        || pv === null || pv === "") {
+// calculates contribution amount
+function calcContri() {
+    if (!validateForm("co-contribution")) {
+        alert("Fill in all required fields.")
         return false;
+    } 
+
+    let rate = document.getElementById("co-rate").value;
+    rate /= 100;
+    let years = document.getElementById('co-years').value;
+    let pv = document.getElementById('co-pv').value;
+    let fv = document.getElementById('co-fv').value;
+    let contribution = (fv - pv * Math.pow((1 + rate), years)) * rate/(Math.pow((1 + rate), years) - 1);
+    let num = Number(contribution.toFixed(2)).toLocaleString();
+    document.getElementById("co-contribution").placeholder = num;
+    return false;
+}
+
+// Validates required form fields are not empty
+function validateForm(field) {
+    var list = ["rate", "years", "pv", "fv", "contribution"];
+    var prefix = field.slice(0,3);
+    var suffix = field.slice(3);
+
+    for (let i = 0; i < list.length; i++) {
+        if (list[i] !== suffix) {
+            var val = document.getElementById(prefix + list[i]).value;
+            if (val === null || val === "") {
+                return false;
+            }
+        }
     }
 
     return true;
+}
+
+// Resets form and associated not required field
+function resetForm(formName, formField) {
+    document.getElementById(formName).reset();
+    document.getElementById(formField).placeholder = "Not required";
 }
